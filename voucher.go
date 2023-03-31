32f04db65f9da5fb57b0761570a1ac545bcf8dc8
Michael Kong <michael.kong@intel.com>
2023-03-31 09:35:42 +0800

    refactor: update types and simplify function
    Signed-off-by: Michael Kong <michael.kong@intel.com>

--- voucher.go
+++ voucher.go
@@ -2,35 +2,26 @@ package main
 
 type Voucher struct {
 	Description      string
-	ValidForDelivery func(distance, weight int) bool
+	ValidForDelivery func(distance, weight float32) bool
 }
 
 var vouchers = map[string]Voucher{
 	"OFR001": {
 		Description: "10% Discount",
-		ValidForDelivery: func(distance, weight int) bool {
-			if distance < 200 && (weight >= 70 && weight <= 200) {
-				return true
-			}
-			return false
+		ValidForDelivery: func(distance, weight float32) bool {
+			return distance < 200 && (weight >= 70 && weight <= 200)
 		},
 	},
 	"OFR002": {
 		Description: "7% Discount",
-		ValidForDelivery: func(distance, weight int) bool {
-			if (distance >= 50 && distance <= 150) && (weight >= 100 && weight <= 250) {
-				return true
-			}
-			return false
+		ValidForDelivery: func(distance, weight float32) bool {
+			return (distance >= 50 && distance <= 150) && (weight >= 100 && weight <= 250)
 		},
 	},
 	"OFR003": {
 		Description: "5% Discount",
-		ValidForDelivery: func(distance, weight int) bool {
-			if (distance >= 50 && distance <= 250) && (weight >= 10 && weight <= 150) {
-				return true
-			}
-			return false
+		ValidForDelivery: func(distance, weight float32) bool {
+			return (distance >= 50 && distance <= 250) && (weight >= 10 && weight <= 150)
 		},
 	},
 }
