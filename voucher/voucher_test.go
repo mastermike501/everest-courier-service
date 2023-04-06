6a42c49a9ab899e89a57a85dff52c35cddb6dd65
Michael Kong <michael.kong@intel.com>
2023-04-06 16:40:41 +0800

    tests: fix tests
    Signed-off-by: Michael Kong <michael.kong@intel.com>

--- voucher/voucher_test.go
+++ voucher/voucher_test.go
@@ -1,70 +1,101 @@
 package voucher
 
 import (
-	"reflect"
 	"testing"
 )
 
-func TestGetVoucherInfo(t *testing.T) {
-	t.Run("Valid voucher code OFR001", func(t *testing.T) {
-		tests := []struct {
-			name string
-			code string
-			want *Voucher
-		}{
-			{
-				name: "Test case 1",
-				code: "OFR001",
-				want: &Voucher{
-					description: "10% Discount",
-					value:       0.1,
-					ValidForDelivery: func(distance, weight float64) bool {
-						return distance < 200 && (weight >= 70 && weight <= 200)
-					},
-				},
-			},
-			{
-				name: "Test case 2",
-				code: "OFR003",
-				want: &Voucher{
-					description: "5% Discount",
-					value:       0.05,
-					ValidForDelivery: func(distance, weight float64) bool {
-						return (distance >= 50 && distance <= 250) && (weight >= 10 && weight <= 150)
-					},
-				},
-			},
-			{
-				name: "Test case 3 - invalid code",
-				code: "OFR004",
-				want: &Voucher{
-					description: "No Discount",
-					value:       0,
-					ValidForDelivery: func(distance, weight float64) bool {
-						return false
-					},
-				},
-			},
-		}
-		for _, tt := range tests {
-			t.Run(tt.name, func(t *testing.T) {
-				if got := GetVoucherInfo(tt.code); !reflect.DeepEqual(got, tt.want) {
-					t.Errorf("getVoucherInfo() = %v, want %v", got, tt.want)
-				}
-			})
-		}
-	})
+func TestVoucher_GetValue(t *testing.T) {
+	tests := []struct {
+		voucher Voucher
+		value   float64
+	}{
+		{Voucher{value: 0.1}, 0.1},
+		{Voucher{value: 0.07}, 0.07},
+		{Voucher{value: 0.05}, 0.05},
+	}
 
-	t.Run("Invalid voucher code", func(t *testing.T) {
-		voucher := GetVoucherInfo("INVALID")
-		if voucher.description != "No Discount" {
-			t.Errorf("Expected voucher.Description to be 'No Discount', but got %v", voucher.description)
+	for _, tt := range tests {
+		if tt.voucher.GetValue() != tt.value {
+			t.Errorf("Expected voucher value %f but got %f", tt.value, tt.voucher.GetValue())
 		}
-		if voucher.value != 0 {
-			t.Errorf("Expected voucher.Value to be 0, but got %v", voucher.value)
+	}
+}
+
+func TestVoucher_IsValid(t *testing.T) {
+	tests := []struct {
+		voucher Voucher
+		valid   bool
+	}{
+		{Voucher{valid: true}, true},
+		{Voucher{valid: false}, false},
+	}
+
+	for _, tt := range tests {
+		if tt.voucher.IsValid() != tt.valid {
+			t.Errorf("Expected voucher validity %t but got %t", tt.valid, tt.voucher.IsValid())
 		}
-		if !voucher.ValidForDelivery(150, 150) {
-			t.Errorf("Expected voucher.ValidForDelivery to return true for any distance and weight")
+	}
+}
+
+func TestGetVoucherInfo(t *testing.T) {
+	tests := []struct {
+		code        string
+		description string
+		value       float64
+		valid       bool
+	}{
+		{"OFR001", "10% Discount", 0.1, true},
+		{"OFR002", "7% Discount", 0.07, true},
+		{"OFR003", "5% Discount", 0.05, true},
+		{"OFR004", "No Discount", 0, false},
+	}
+
+	for _, tt := range tests {
+		voucher := GetVoucherInfo(tt.code)
+
+		if voucher.description != tt.description {
+			t.Errorf("Expected voucher description %s but got %s", tt.description, voucher.description)
 		}
-	})
+
+		if voucher.value != tt.value {
+			t.Errorf("Expected voucher value %f but got %f", tt.value, voucher.value)
+		}
+
+		if voucher.valid != tt.valid {
+			t.Errorf("Expected voucher validity %t but got %t", tt.valid, voucher.valid)
+		}
+	}
+}
+
+func TestVoucher_ValidForDelivery(t *testing.T) {
+	testCases := []struct {
+		voucherCode  string
+		distance     float64
+		weight       float64
+		expectedBool bool
+	}{
+		{voucherCode: "OFR001", distance: 100, weight: 100, expectedBool: true},
+		{voucherCode: "OFR001", distance: 100, weight: 70, expectedBool: true},
+		{voucherCode: "OFR001", distance: 199, weight: 200, expectedBool: true},
+		{voucherCode: "OFR001", distance: 200, weight: 150, expectedBool: false},
+		{voucherCode: "OFR002", distance: 49, weight: 200, expectedBool: false},
+		{voucherCode: "OFR002", distance: 50, weight: 99, expectedBool: false},
+		{voucherCode: "OFR002", distance: 50, weight: 100, expectedBool: true},
+		{voucherCode: "OFR002", distance: 150, weight: 250, expectedBool: true},
+		{voucherCode: "OFR002", distance: 151, weight: 250, expectedBool: false},
+		{voucherCode: "OFR003", distance: 49, weight: 150, expectedBool: false},
+		{voucherCode: "OFR003", distance: 50, weight: 9, expectedBool: false},
+		{voucherCode: "OFR003", distance: 50, weight: 10, expectedBool: true},
+		{voucherCode: "OFR003", distance: 250, weight: 150, expectedBool: true},
+		{voucherCode: "OFR003", distance: 251, weight: 150, expectedBool: false},
+		{voucherCode: "invalid", distance: 100, weight: 100, expectedBool: false},
+	}
+
+	for _, tc := range testCases {
+		voucher := GetVoucherInfo(tc.voucherCode)
+		actualBool := voucher.ValidForDelivery(tc.distance, tc.weight)
+		if actualBool != tc.expectedBool {
+			t.Errorf("Expected ValidForDelivery() to return %v for voucher %s with distance %f and weight %f; got %v", tc.expectedBool, tc.voucherCode, tc.distance, tc.weight, actualBool)
+		}
+	}
 }
