5fb92353d2052c9608873e2b3756ce631f776187
Michael Kong <michael.kong@intel.com>
2023-04-06 14:25:50 +0800

    feat: update Package struct
    Signed-off-by: Michael Kong <michael.kong@intel.com>

--- cmd/courier-service/package.go
+++ cmd/courier-service/package.go
@@ -1,16 +1,34 @@
 package main
 
+import (
+	"fmt"
+	"math"
+)
+
 type Package struct {
-	Name         string
-	Weight       float64
-	Distance     float64
-	OfferCode    string
-	DeliveryTime float64
+	Name      string
+	Weight    float64
+	Distance  float64
+	Discount  float64
+	TotalCost float64
+
+	TimeToDest   float64 // exact time taken from depot to dest based on vehicle speed
+	DeliveryTime float64 // TimeToDest + current time
+}
+
+// pkg_id1 discount1 total_cost1 estimated_delivery_time1_in_hours
+func (p *Package) Println() {
+	fmt.Printf("%s %.0f %.0f %.2f\n", p.Name, p.Discount, p.TotalCost, p.DeliveryTime)
+}
+
+func (p *Package) CalculateTimeToDest(speed int) {
+	t := p.Distance / float64(speed)
+	p.TimeToDest = math.Floor(t*100) / 100 // rounds down to 2 dec places
 }
 
 type Shipment struct {
-	DeliveryTime float64
-	Packages     map[string]*Package
+	OneWayDeliveryTime float64
+	Packages           map[string]*Package
 }
 
 func (s *Shipment) addPackages(pkgs []*Package) {
@@ -22,8 +40,8 @@ func (s *Shipment) addPackages(pkgs []*Package) {
 	// calculate the delivery time of this shipment, which is
 	// max(Packages.DeliveryTime)
 	for _, p := range s.Packages {
-		if p.DeliveryTime > s.DeliveryTime {
-			s.DeliveryTime = p.DeliveryTime
+		if p.TimeToDest > s.OneWayDeliveryTime {
+			s.OneWayDeliveryTime = p.TimeToDest
 		}
 	}
 }
